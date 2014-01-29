e284b7e0c7f2e938ff8718e407825a45420f1f9f
Matt Bocek <mbocek@gmail.com>
2014-01-28 22:02:27 -0800

    Print gallery(static)
    Totally static page with a gallery of print work

---
+++ print/gallery.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<head>
+	<!-- Jquery -->
+	<script src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
+
+	<!-- Bootstrap -->
+	<link href="http://netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css" rel="stylesheet">
+	<script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
+
+	<title>Porfolio | Matthew Bocek</title>
+    
+    <style type="text/css">
+        
+        .contact img, .control img {
+            width: 40px;
+        }
+        
+        .control a {
+            width: 100%;
+        }
+        
+        p.control {
+            padding-top: 0.5em;
+        }
+    </style>
+    
+</head>
+<body>
+	<div class="container" style="">
+		<div class="row contact">
+            <a href="http://mbocek.comxa.com">
+                <h1 class="col-md-6">
+                    Matt Bocek - web dev portfolio
+                </h1>
+            </a>
+            <p class="col-md-1 col-md-offset-2">
+                <a class="btn btn-default" href="../mbocek-resume.pdf">
+                    <img class="" src="../images/resume.png"/>
+                </a>
+            </p>
+            <p class="col-md-1">
+                <a class="btn btn-default" href="http://www.github.com/matthewbocek">
+                    <img class="" src="../images/GitHub-Mark-32px.png"/>
+                </a>
+            </p>
+            <p class="col-md-1">
+                <a class="btn btn-default" href="http://www.linkedin.com/pub/matthew-bocek/26/482/a0b">
+                    <img class=""  src="../images/linkedin.png"/>
+                </a>
+            </p>
+            <p class="col-md-1">
+                <a class="btn btn-default" href="mailto:mbocek@gmail.com" >
+                        <img src="../images/mail.png"/>
+                </a>
+            </p>
+
+
+        </div>
+        <div class="row print-images">
+            <p>
+                <img src="images/unity_fair_2.jpg" />
+                <img src="images/unity_fair_mainstage.jpg" />
+                <img src="images/alexie.jpg" />
+            </p>
+            
+            <p>
+                <img src="images/fall_forum.jpg" />
+                <img src="images/pingpong.jpg" />
+                <img src="images/entrep.jpg" />
+            </p>
+			
+        </div>
+    </div>
+</body>
+</html>
